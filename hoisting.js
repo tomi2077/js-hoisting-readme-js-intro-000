1db50cf9aa590b33b563386c0b56cf9db798c8c1
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 14:09:42 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,18 +11,16 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
   sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
